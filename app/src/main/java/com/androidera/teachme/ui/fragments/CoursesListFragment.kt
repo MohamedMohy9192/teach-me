e489ffa49809f81aa28157a5ecb2a14d9ab54ef3
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-12-21 00:08:11 +0200

    Fix view binding setup to show the courses list

--- app/src/main/java/com/androidera/teachme/ui/fragments/CoursesListFragment.kt
+++ app/src/main/java/com/androidera/teachme/ui/fragments/CoursesListFragment.kt
@@ -5,13 +5,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.androidera.teachme.R
 import com.androidera.teachme.adapters.CoursesAdapter
+import com.androidera.teachme.databinding.FragmentCoursesListBinding
 import com.androidera.teachme.ui.CoursesActivity
 import com.androidera.teachme.ui.CoursesViewModel
 import com.androidera.teachme.util.Resource
@@ -19,7 +16,8 @@ import com.androidera.teachme.util.Resource
 class CoursesListFragment : Fragment() {
 
     private lateinit var viewModel: CoursesViewModel
-    //private var fragmentCoursesListBinding: FragmentCoursesListBinding? = null
+    private var _binding : FragmentCoursesListBinding? = null
+    private val binding get() = _binding!!
     private lateinit var coursesAdapter: CoursesAdapter
 
     private val TAG = CoursesListFragment::class.simpleName
@@ -27,76 +25,57 @@ class CoursesListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_courses_list, container, false)
+    ): View {
+        _binding = FragmentCoursesListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       // val binding = FragmentCoursesListBinding.bind(view)
-        //fragmentCoursesListBinding = binding
-
         viewModel = (activity as CoursesActivity).viewModel
 
-        setupRecyclerView(view)
-        Log.d(
-            TAG,
-            "CoursesListFragment"
-        )
+        setupRecyclerView()
 
-        viewModel.courses.observe(viewLifecycleOwner, Observer { response ->
-
-            Log.d(
-                TAG,
-                "Response In CoursesListFragment: courses.observe ${response.data?.next}"
-            )
-
+        viewModel.courses.observe(viewLifecycleOwner, { response ->
             when (response) {
                 is Resource.Success -> {
-                    hideProgressBar(view)
+                    hideProgressBar()
                     response.data?.let { coursesResponse ->
-                        Log.d(
-                            TAG,
-                            "Response In CoursesListFragment: courses.observe ${coursesResponse.next}"
-                        )
                         coursesAdapter.differ.submitList(coursesResponse.results)
-
-
                     }
                 }
                 is Resource.Error -> {
-                    hideProgressBar(view)
+                    hideProgressBar()
                     response.message?.let { message ->
                         Log.e(TAG, "An error occurred: $message")
                     }
                 }
                 is Resource.Loading -> {
-                    showProgressBar(view)
+                    showProgressBar()
                 }
             }
         })
     }
 
-    private fun hideProgressBar(view: View) {
-        view.findViewById<ProgressBar>(R.id.pagination_progress_bar)?.visibility = View.INVISIBLE
+    private fun hideProgressBar() {
+       binding.paginationProgressBar.visibility = View.INVISIBLE
     }
 
-    private fun showProgressBar(view: View) {
-        view.findViewById<ProgressBar>(R.id.pagination_progress_bar)?.visibility = View.VISIBLE
+    private fun showProgressBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
     }
 
-    private fun setupRecyclerView(view: View) {
+    private fun setupRecyclerView() {
         coursesAdapter = CoursesAdapter()
-        view.findViewById<RecyclerView>(R.id.courses_recycler_view)?.apply {
+        binding.coursesRecyclerView.apply {
             adapter = coursesAdapter
             layoutManager = LinearLayoutManager(activity)
         }
     }
 
-  /*  override fun onDestroyView() {
-        fragmentCoursesListBinding = null
+    override fun onDestroyView() {
         super.onDestroyView()
-    }*/
+        _binding = null
+    }
 }
