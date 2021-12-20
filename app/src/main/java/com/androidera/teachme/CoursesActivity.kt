28acf5a30c2b3aeb1e5aef27faaa41da3a72ee1c
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-12-20 02:19:00 +0200

    Setup bottom navigation with navigation component

--- app/src/main/java/com/androidera/teachme/CoursesActivity.kt
+++ app/src/main/java/com/androidera/teachme/CoursesActivity.kt
@@ -2,10 +2,24 @@ package com.androidera.teachme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.androidera.teachme.databinding.ActivityCoursesBinding
 
 class CoursesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCoursesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_courses)
+        binding = ActivityCoursesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.coursesNavHostFragment
+        ) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
