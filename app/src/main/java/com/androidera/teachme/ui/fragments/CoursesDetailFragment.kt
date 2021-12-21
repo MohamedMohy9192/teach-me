4999f88eb33fca5744bb3a2232ab30c4150ac3d5
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-12-21 05:57:05 +0200

    Refactor open course URL in external browser instead of web view

--- app/src/main/java/com/androidera/teachme/ui/fragments/CoursesDetailFragment.kt
+++ app/src/main/java/com/androidera/teachme/ui/fragments/CoursesDetailFragment.kt
@@ -1,15 +1,15 @@
 package com.androidera.teachme.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.androidera.teachme.R
 import com.androidera.teachme.databinding.FragmentCoursesDetailBinding
+import com.androidera.teachme.util.Constants
 
 class CoursesDetailFragment : Fragment() {
     private var _binding: FragmentCoursesDetailBinding? = null
@@ -30,11 +30,12 @@ class CoursesDetailFragment : Fragment() {
         val course = args.course
 
         binding.openCoursesViewFragmentButton.setOnClickListener {
-            val bundle = bundleOf("courseUrl" to course.url)
-            findNavController().navigate(
-                R.id.action_coursesDetailFragment_to_coursesViewFragment,
-                bundle
-            )
+            val webpage = Uri.parse(Constants.UDEMY_BASE_URL + course.url)
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            if (intent.resolveActivity(requireActivity().packageManager) != null){
+                startActivity(intent)
+            }
+
         }
 
     }
