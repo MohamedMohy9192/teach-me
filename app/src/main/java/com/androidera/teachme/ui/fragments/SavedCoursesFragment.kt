4dffcfa42cd69da9972a7fc627e3c7953fd0db93
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-12-21 06:47:01 +0200

    Add swipe to delete saved courses

--- app/src/main/java/com/androidera/teachme/ui/fragments/SavedCoursesFragment.kt
+++ app/src/main/java/com/androidera/teachme/ui/fragments/SavedCoursesFragment.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.androidera.teachme.R
 import com.androidera.teachme.adapters.CoursesAdapter
 import com.androidera.teachme.databinding.FragmentSavedCoursesBinding
 import com.androidera.teachme.ui.CoursesActivity
 import com.androidera.teachme.ui.CoursesViewModel
-import com.androidera.teachme.util.Constants
 import com.androidera.teachme.util.Constants.Companion.COURSE_BUNDLE_KEY
+import com.google.android.material.snackbar.Snackbar
 
 class SavedCoursesFragment : Fragment() {
 
@@ -44,6 +47,38 @@ class SavedCoursesFragment : Fragment() {
                 bundle
             )
         }
+
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val course = coursesAdapter.differ.currentList[position]
+                viewModel.deleteCourse(course)
+                Snackbar.make(view, "Successfully deleted course", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo") {
+                        viewModel.saveCourse(course)
+                    }
+                    show()
+                }
+            }
+        }
+
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(binding.savedCoursesRecyclerView)
+        }
+        viewModel.getSavedCourses().observe(viewLifecycleOwner, Observer { courses ->
+            coursesAdapter.differ.submitList(courses)
+        })
     }
 
     private fun setupRecyclerView() {
