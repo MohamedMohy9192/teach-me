96529f4ad144d5e8d0cccd728feba97c2c69e696
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-01-02 15:11:15 +0200

    Setup default action bar with navigation controller to display fragments labels

--- app/src/main/java/com/androidera/teachme/ui/CoursesActivity.kt
+++ app/src/main/java/com/androidera/teachme/ui/CoursesActivity.kt
@@ -3,7 +3,11 @@ package com.androidera.teachme.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androidera.teachme.R
 import com.androidera.teachme.data.CoursesDatabase
@@ -14,11 +18,13 @@ class CoursesActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCoursesBinding
     lateinit var viewModel: CoursesViewModel
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCoursesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         val coursesRepository = CoursesRepository(CoursesDatabase(this))
         val viewModelProviderFactory =
@@ -31,6 +37,13 @@ class CoursesActivity : AppCompatActivity() {
             R.id.coursesNavHostFragment
         ) as NavHostFragment
         val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.coursesNavHostFragment).navigateUp(appBarConfiguration) ||
+                super.onSupportNavigateUp()
     }
 }
